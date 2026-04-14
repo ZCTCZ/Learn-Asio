de0424380fdfac4b496e9c8dc7d90acbebe65d53
zctcz <2932742577@qq.com>
2026-04-15 01:56:22 +0800

    async_servr，通过boost的异步信号处理函数，实现了服务器的完美退出

--- TestAsyncServer/main.cpp
+++ TestAsyncServer/main.cpp
@@ -6,7 +6,9 @@ int main()
     try
     {
         constexpr int MAX_LEN = 1024 * 10; // 发送的数据包的最大长度
-        constexpr int HEAD_LEN = 4; // 数据包首部长度
+        constexpr int HEAD_DATA_LEN = 2; // 数据包首部里表示发送的数据的长度的字段所占字节数
+        constexpr int HEAD_ID_LEN = 2; // 数据包首部里表示发送的数据的id的字段所占字节数
+        constexpr int HEAD_TOTAL_LEN = 4;// 数据包首部长度
         boost::asio::io_context ioc;
         boost::system::error_code ec;
         boost::asio::ip::tcp::socket socket(ioc);
@@ -23,22 +25,25 @@ int main()
         {
             try
             {
-                char data_len[HEAD_LEN] = {'\0'};
-                char buf[MAX_LEN] = {'\0'};
+                char head[HEAD_TOTAL_LEN + 1] = {'\0'};
+                char buf[MAX_LEN + 1] = {'\0'};
                 for (;;)
                 {
-                    /// 先读取 HEAD_LEN 字节的报文首部
-                    boost::asio::read(socket, boost::asio::buffer(data_len, HEAD_LEN));
-                    uint32_t len = 0;
-                    memcpy(&len, data_len, HEAD_LEN);
-                    len = boost::asio::detail::socket_ops::network_to_host_long(len);
-                    /// 再读取 data_len 长度的报文体
-                    boost::asio::read(socket, boost::asio::buffer(buf, len));
+                    boost::asio::read(socket, boost::asio::buffer(head, HEAD_TOTAL_LEN));
+                    uint16_t msg_id = 0;
+                    memcpy(&msg_id, head, HEAD_ID_LEN);
+                    msg_id = boost::asio::detail::socket_ops::network_to_host_short(msg_id);
 
-                    std::cout << buf << std::endl;
+                    uint16_t data_len = 0;
+                    memcpy(&data_len, head + HEAD_ID_LEN, HEAD_DATA_LEN);
+                    data_len = boost::asio::detail::socket_ops::network_to_host_short(data_len);
 
-                    memset(buf, '\0', len);
-                    memset(data_len, '\0', HEAD_LEN);
+                    boost::asio::read(socket, boost::asio::buffer(buf, data_len));
+
+                    std::cout << '[' << msg_id << ']' << buf << std::endl;
+
+                    memset(buf, '\0', data_len);
+                    memset(head, '\0', HEAD_TOTAL_LEN);
                     std::this_thread::sleep_for(std::chrono::milliseconds(10));
                 }
             }
@@ -55,17 +60,27 @@ int main()
             try
             {
                 long long cnt {0};
-                char send_data[MAX_LEN] = {'\0'};
+                long long id = 1001;
+                char send_data[MAX_LEN] = {};
                 for (;;)
                 {
-                    std::string data = "Hello World!" + std::to_string(cnt++);
-                    uint32_t data_len = data.size();
-                    uint32_t tmp = boost::asio::detail::socket_ops::host_to_network_long(data_len);
-                    memcpy(send_data, &tmp, HEAD_LEN);
-                    memcpy(send_data + HEAD_LEN, data.c_str(), data_len);
-                    boost::asio::write(socket, boost::asio::buffer(send_data, data_len + HEAD_LEN));
+                    /// 构造首部里的id字段
+                    uint16_t tmp = boost::asio::detail::socket_ops::host_to_network_short(id++);
+                    memcpy(send_data, &tmp, HEAD_ID_LEN);
 
-                    memset(send_data, '\0', data_len + HEAD_LEN);
+                    /// 构造待发送的数据
+                    std::string data = "你好 World!" + std::to_string(cnt++);
+
+                    /// 构造首部里表示数据长度的字段
+                    uint16_t data_len = data.size();
+                    tmp = boost::asio::detail::socket_ops::host_to_network_short(data_len);
+                    memcpy(send_data + HEAD_ID_LEN, &tmp, HEAD_DATA_LEN);
+
+                    /// 构造数据报的报体
+                    memcpy(send_data + HEAD_TOTAL_LEN, data.c_str(), data_len);
+                    boost::asio::write(socket, boost::asio::buffer(send_data, data_len + HEAD_TOTAL_LEN));
+
+                    memset(send_data, '\0', data_len + HEAD_TOTAL_LEN);
                     std::this_thread::sleep_for(std::chrono::milliseconds(10));// 休眠10毫秒
                 }
             }
