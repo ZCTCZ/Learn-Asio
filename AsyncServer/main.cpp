4d6474c47903f28a37481e29f4d7f1045cd74b68
zctcz <2932742577@qq.com>
2026-04-15 15:09:12 +0800

    async_servr，通过boost的异步信号处理函数，实现了服务器的完美退出

--- AsyncServer/main.cpp
+++ AsyncServer/main.cpp
@@ -2,18 +2,37 @@
 
 #include "Server.h"
 
-int main() {
+int main()
+{
     try
     {
         boost::asio::io_context ioc;
+        boost::asio::signal_set signals(ioc, SIGINT, SIGTERM, SIGABRT);
+        signals.async_wait([&ioc](const boost::system::error_code& error, int signal_num)
+        {
+            if (error)
+            {
+                std::cerr << "Occurred Error When Capture Signal!"
+                    << " error_code = " << error.value()
+                    << " error_message = " << error.message() << std::endl;
+            }
+            else
+            {
+                std::cout << "Capture Signal:" << signal_num << '\n'
+                    << "服务器完美退出！" << std::endl;
+            }
+
+            ioc.stop();
+        });
+        
         Server s(ioc, 60001);
         ioc.run(); // 启动底层的 I/O 多路复用
     }
-    catch (boost::system::system_error &error)
+    catch (boost::system::system_error& error)
     {
         std::cerr << "Occurred Error"
-        << " error_code = " << error.code().value()
-        << " error_message = " << error.what() << std::endl;
+            << " error_code = " << error.code().value()
+            << " error_message = " << error.what() << std::endl;
     }
     return 0;
 }
